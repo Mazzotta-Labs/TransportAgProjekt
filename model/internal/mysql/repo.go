6a5955a65c752d5d64cda272449da4e8abd8b5a3
sandro.zogg@edu.teko.ch <sandro.zogg@edu.teko.ch>
2021-12-08 15:49:31 +0100

    [+] Entitys definiert

--- model/internal/mysql/repo.go
+++ model/internal/mysql/repo.go
@@ -1,10 +1,68 @@
 package mysql
 
 import (
+	"TransportAgProjekt1/model/entity"
 	"database/sql"
+	"fmt"
 )
 
 var db *sql.DB
+
+type MySqlRepository struct {
+	drivers []entity.Driver
+}
+
+func NewMySqlRepository() *MySqlRepository {
+	repository := MySqlRepository{}
+	db = openDatabase()
+	repository.drivers = repository.FindAllDriver()
+	return &repository
+}
+
+func (r *MySqlRepository) FindAllDriver() []entity.Driver {
+	var drivers []entity.Driver
+	result, err := db.Query("select * from Driver")
+	fmt.Println(result)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for result.Next() {
+		var driver entity.Driver
+
+		err := result.Scan(&driver.in, &book.Title, &book.Author, &book.PublishedYear, &book.Borrowed)
+		if err != nil {
+			panic(err.Error())
+		}
+		drivers = append(drivers, driver)
+	}
+	return drivers
+}
+
+func (r *MySqlRepository) AddDriver(driver entity.Driver) {
+	isbn := book.ISBN
+	title := book.Title
+	author := book.Author
+	year := book.PublishedYear
+	borrowed := book.Borrowed
+
+	stmt, err := db.Prepare("insert into model values (?,?,?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(isbn, title, author, year, borrowed)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (r *MySqlRepository) UpdateDriver(driver entity.Driver) {
+	//TODO
+}
+
+func (r *MySqlRepository) DeleteDriver(driver entity.Driver) {
+	//TODO
+}
 
 /*
 type MySqlRepository struct {
@@ -68,7 +126,7 @@ func (r *MySqlRepository) UpdateBook(book entity.Book) {
 */
 
 func openDatabase() *sql.DB {
-	db, err := sql.Open("mysql", "root:Gamer-zone22@tcp(localhost:3306)/mybooks")
+	db, err := sql.Open("mysql", "root:12345@tcp(localhost:3306)/mybooks")
 	if err != nil {
 		panic(err.Error())
 	}
