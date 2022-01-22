35af9370c18eb37ea7696aec9d0e7db821d7b778
sandro.zogg@edu.teko.ch <sandro.zogg@edu.teko.ch>
2022-01-22 11:29:02 +0100

    [*] server version

--- cli/bot1.go
+++ cli/bot1.go
@@ -31,17 +31,17 @@ func printBotOrderList(ordersToPrint []entity.Order) string {
 	txt = "***** Auftrag Print *****\n"
 	for i, order := range ordersToPrint {
 		txt = txt + "Auftrag " + toStr(i+1) + "\n" +
-			"Auftrags Nr.:" + toStr(order.OrderId) + ",\n" +
-			"Datum:" + order.OrderDate + ",\n" +
-			"Name:" + order.CustomerPrename + ",\n" +
-			"Vorname:" + order.CustomerName + ",\n" +
-			"PLZ:" + order.Plz + ",\n" +
-			"Ort:" + order.Town + ",\n" +
-			"Strasse:" + order.Street + ",\n" +
-			"Land:" + order.Country + ",\n" +
-			"Fahrer Name:" + order.Name + ",\n" +
-			"Fahrer Vorame:" + order.Prename + ",\n" +
-			"Fahrzeug Nr.:" + order.Number + ",\n"
+			"Auftrags Nr.: " + toStr(order.OrderId) + ",\n" +
+			"Datum: " + order.OrderDate + ",\n" +
+			"Name: " + order.CustomerPrename + ",\n" +
+			"Vorname: " + order.CustomerName + ",\n" +
+			"PLZ: " + order.Plz + ",\n" +
+			"Ort: " + order.Town + ",\n" +
+			"Strasse: " + order.Street + ",\n" +
+			"Land: " + order.Country + ",\n" +
+			"Fahrer Name: " + order.Name + ",\n" +
+			"Fahrer Vorame :" + order.Prename + ",\n" +
+			"Fahrzeug Nr. :" + order.Number + ",\n" + "\n"
 	}
 	return txt
 }
