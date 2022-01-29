d546a230d75b1e0346a18a1d68027ea539548ed9
sandro.zogg@edu.teko.ch <sandro.zogg@edu.teko.ch>
2022-01-29 09:50:20 +0100

    [*] server version

--- model/internal/mysql/repo_order.go
+++ model/internal/mysql/repo_order.go
@@ -9,6 +9,8 @@ func (r *MySqlRepository) FindAllOrder() []entity.Order {
 	var orders []entity.Order
 	result, err := db.Query("select o.id, o.order_date, c.name, c.prename, a.street, a.plz, a.town, a.country, d.name, d.prename, v.number from `order` o left join customer c on c.id = o.customer_id left join address a on c.address_id = a.id left join driver d on d.id = o.driver_id left join vehicle v on d.vehicle_id = v.id left join ordertoproduct op on op.order_id = o.id left join product p on p.id = op.product_id order by o.id")
 	if err != nil {
+		println(err)
+		time.Sleep(10 * time.Second)
 		panic(err.Error())
 	}
 
